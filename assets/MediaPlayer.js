7c0a22cb4ab85b3b0d1bb0b1619e484c642daddf
Gustavo M <gamaades@gmail.com>
2022-07-08 18:21:23 -0400

    comenzando con los closures

---
+++ assets/MediaPlayer.js
@@ -0,0 +1,23 @@
+function MediaPlayer(config) {
+    this.media = config.el;
+}
+
+MediaPlayer.prototype.play = function(){
+    this.media.play();
+}
+
+MediaPlayer.prototype.pause = function(){
+    this.media.pause();
+}
+
+MediaPlayer.prototype.togglePlay = function(){
+    if(this.media.paused){
+    this.media.play();
+    } else {
+    this.media.pause();
+    }
+    // o podemos usar lo siguiente:
+    // this.media.paused ? this.media.play() : this.media.pause()
+};
+
+export default MediaPlayer;
--- assets/index.js
+++ assets/index.js
@@ -1,19 +1,7 @@
+import MediaPlayer from "./MediaPlayer.js"
+
 const video = document.querySelector("video");
-const button = document.querySelector("button");;
-let click = 0;
-function MediaPlayer(config) {
-    this.media = config.el;
-}
-
-MediaPlayer.prototype.play = function(){
-    if(this.media.paused){
-    this.media.play();
-    } else {
-    this.media.pause();
-    }
-    // o podemos usar lo siguiente:
-    // this.media.paused ? this.media.play() : this.media.pause()
-};
-
 const player = new MediaPlayer({el: video});
-button.onclick = () => player.play();
+
+const button = document.querySelector("button");;
+button.onclick = () => player.togglePlay();
