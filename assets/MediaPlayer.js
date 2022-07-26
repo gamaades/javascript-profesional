2f9f21932b6670fcd638077536e227ba3c0d7d98
gamaades <gamaades@gmail.com>
2022-07-25 21:53:48 -0400

    media player

--- assets/MediaPlayer.js
+++ assets/MediaPlayer.js
@@ -6,12 +6,26 @@ function MediaPlayer(config) {
 }
 
 MediaPlayer.prototype._initPlugins = function() {
+    const player = {
+        play: () => this.play(),
+        pause: () => this.pause(),
+        media: this.media,
+        
+        get muted() {
+            return this.media.muted;
+        },
+
+        set muted(value) {
+            this.media.muted = false;
+        }
+    };
+
     this.plugins.forEach(plugin => {
-       plugin.run(this) 
+       plugin.run(player);
     });
 }
 
-MediaPlayer.prototype.play = function(){
+MediaPlayer.prototype.play = function() {
     this.media.play();
 }
 
